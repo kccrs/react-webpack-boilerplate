61484fd7f763f06921488b37d56c63f5c2c4c002
swanie21 <kswanie21@gmail.com>
2016-12-21 23:22:36 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: {
+    main: './lib/index.js',
+  },
+  output: {
+    path: __dirname,
+    filename: 'public/[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!resolve-url!sass?sourceMap' },
+      { test: /\.png$/, loader: 'url-loader', query: { mimetype: 'image/png'} },
+      { test: /\.jpg$/, loader: 'url-loader', query: { mimetype: 'image/jpg'} },
+      { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel',
+        query: { presets: ['es2015', 'react'], },
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+};
